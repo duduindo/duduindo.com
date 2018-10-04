24b05057c560e99ace679cff6c11c20280016205
Eduardo Paixão <eduardo.paixao@grupofolha.com.br>
2018-10-04 17:23:28 -0300

    Gerado sem cache

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,12 @@
 
+if ('serviceWorker' in navigator) {
+  const sw = navigator.serviceWorker.register('/serviceworker.js');
+
+  sw.then(function(registration) {
+    console.log('Service Worker registered with scope:', registration.scope);
+  });
+
+  sw.catch(function(err) {
+    console.log('Service Worker registration failed:', err);
+  });
+}
